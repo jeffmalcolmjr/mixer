f3deb7137667b6cfdfb1690aa3de328314442684
Jeffmalcolmjr <jeffmalcolmjr@gmail.com>
2022-10-19 10:17:06 -0400

    Fixing Bugs
    An error occurred if the user entered a number outside of the RGB ranges.

--- mixer.js
+++ mixer.js
@@ -3,9 +3,10 @@ function changeColor() {
     var rValue = parseInt(document.getElementById("rValue").value);
     var gValue = parseInt(document.getElementById("gValue").value);
     var bValue = parseInt(document.getElementById("bValue").value);
+    var run = true;
 //Checks to see if the number is within the ranges of 0-255
     if ((rValue > 255 || bValue > 255 || gValue > 255) || (rValue < 0 || bValue < 0 || gValue < 0)) {
-        alert("Invalid Number. Please enter a number between 0-255")
+        run = false;
     }
 //Makes the text white if the background color is too dark
     if ((rValue < 45 ) || (bValue < 45) || (gValue < 45)) {
@@ -26,10 +27,13 @@ function changeColor() {
         const bold = document.getElementById("bold")
         bold.style.color = 'black'
     }
-//Converts the numbers to an rgb(0,0,0) for the CSS to read
-    var newColor = "rgb("+(rValue)+"," +(gValue)+","+bValue+")"
-//Sets the background equal to the color
-    document.body.style.background = newColor
+    if(run == true) {
+        var newColor = "rgb("+(rValue)+"," +(gValue)+","+bValue+")"
+        //Sets the background equal to the color
+        document.body.style.background = newColor
+    } else {
+        alert("Invalid Number. Please enter a number between 0-255")
+    }
 
 }
 
