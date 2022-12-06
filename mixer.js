a54e55edb6f4b541479f5006e0b89164c0eaa0fa
Jeffmalcolmjr <jeffmalcolmjr@gmail.com>
2022-12-06 00:55:36 -0500

    Updates Javascript for Color Mixer

--- mixer.js
+++ mixer.js
@@ -3,48 +3,37 @@ function changeColor() {
     var rValue = parseInt(document.getElementById("rValue").value);
     var gValue = parseInt(document.getElementById("gValue").value);
     var bValue = parseInt(document.getElementById("bValue").value);
-    var run = true;
-//Checks to see if the number is within the ranges of 0-255
-    if ((rValue > 255 || bValue > 255 || gValue > 255) || (rValue < 0 || bValue < 0 || gValue < 0)) {
-        run = false;
-    }
+//Checks to see if the number is within the ranges of 0-25 
+   //checkVisibility(rValue,bValue,gValue)
+    //Converts the numbers to an rgb(0,0,0) for the CSS to read
+    var newColor = "rgb("+(rValue)+"," +(gValue)+","+bValue+")"
+    //Sets the background equal to the color
+    document.body.style.background = newColor
+    checkVisibility(rValue,gValue,bValue)
+    var rHex = rValue.toString(16).toUpperCase();
+    var gHex = gValue.toString(16).toUpperCase();
+    var bHex = bValue.toString(16).toUpperCase();
+    document.getElementById("hexOutput").innerHTML = "#"+rHex+gHex+bHex;
+}
 //Makes the text white if the background color is too dark
-    if ((rValue < 45 ) || (bValue < 45) || (gValue < 45)) {
-        const header = document.getElementById("head")
+function checkVisibility(rValue,gValue,bValue) {
+    const header = document.getElementById("head")
+    const bold = document.getElementById("bold")
+    const footer = document.getElementById("footer")
+    if ((rValue < 70 ) && (bValue < 70) && (gValue < 70)) {
         header.style.color = 'white'
-        const bold = document.getElementById("bold")
-        bold.style.color = 'white'
+        bold.style.color = 'white'    
+        footer.style.color = 'white'  
     }
-    if (!((rValue < 60 ) && (bValue < 60) && (gValue < 60))) {
-        const header = document.getElementById("head")
+    if (!((rValue < 70 ) && (bValue < 70) && (gValue < 70))) {
         header.style.color = 'black'
-        const bold = document.getElementById("bold")
         bold.style.color = 'black'
+        footer.style.color = 'black' 
     }
     if((rValue > 199) && (bValue > 199) && (gValue > 199)) {
-        const header = document.getElementById("head")
         header.style.color = 'black'
-        const bold = document.getElementById("bold")
         bold.style.color = 'black'
+        footer.style.color = 'black' 
     }
-    if(run == true) {
-        var newColor = "rgb("+(rValue)+"," +(gValue)+","+bValue+")"
-        //Sets the background equal to the color
-        document.body.style.background = newColor
-    } else {
-        alert("Invalid Number. Please enter a number between 0-255")
-    }
-
-}
-
-function reset() {
-    document.body.style.background = 'white'
-    var header = document.getElementById("head")
-    header.style.color = 'black'
-    var bold = document.getElementById("bold")
-    bold.style.color = 'black'
-    document.getElementById("rValue").value = ''
-    document.getElementById("bValue").value = ''
-    document.getElementById("gValue").value = ''
 }
 
